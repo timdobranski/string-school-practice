5e4a8738b20c5e4dbe907552cb19f5c409393ea1
Tim <timdobranski@gmail.com>
2023-07-09 21:00:30 -0700

    Added logo for practice with Image components

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const windowWidth = Dimensions.get('window').width;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.logo}
+        source={require('./assets/lmss-logo.png')}
+        resizeMode='contain'/>
+      <Text style={styles.text}>Welcome to the String School mobile app!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    width: '85%',
+    position: 'absolute',
+    top: 0,
+  },
+  text:{
+    color: 'white',
+    fontSize: 20,
+  }
 });
